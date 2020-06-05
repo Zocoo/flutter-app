978fa83cdbf0cd7d210085ca3d94ae0258903c44
Zocoo <815769472@qq.com>
2020-06-05 17:56:35 +0800

    a
    a

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,6 +1,6 @@
 class Config {
   String host = "http://ddz.so-what.cc";
   int version = 11;
-  String ossKey = '6f9UuyAgFQnJaT3C3YoJKZaAoWMfyM';
+  String ossKey = '6f9UuyAgFQfsfnJaffT3C3aYoJ';
 //  String host = "http://so-what.nat123.cc";
 }
